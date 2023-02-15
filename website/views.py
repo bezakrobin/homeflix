b594e9f2c081f7e976a1672901ed46e5e549625e
Robin Bezak <76775905+bezakrobin@users.noreply.github.com>
2023-02-15 04:25:57 +0100

    commented getFromImdb()

--- website/views.py
+++ website/views.py
@@ -9,7 +9,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 @views.route('/')
 def home():
     header = getNewest()
-    data = getFromImdb(header['id'])
+    # data = getFromImdb(header['id'])
     return render_template('home.html', header = header)
 
 def getNewest():
@@ -21,21 +21,21 @@ def getNewest():
 # getting ready for adding a film to db, downloading trailer and poster
 # make it without needing id, send in just imdb url, and the program will care about all
 # prepare for connecting to bombuj, europix or something else, also prepare for the torrent solution
-def getFromImdb(id):
-    movies = requests.get('http://localhost:3000/movies/' + str(id))
-    url = movies.json()['imdb-url']
-    imdb = requests.get(url, headers = headers)
-    soup = BeautifulSoup(imdb.text, "html.parser")
-    script = soup.find(id = '__NEXT_DATA__')
-    data = json.loads(script.get_text())
-    title = data['props']['pageProps']['aboveTheFoldData']['originalTitleText']['text']
-    year = data['props']['pageProps']['aboveTheFoldData']['releaseYear']['year']
-    length = data['props']['pageProps']['aboveTheFoldData']['runtime']['displayableProperty']['value']['plainText']
-    rating = data['props']['pageProps']['aboveTheFoldData']['ratingsSummary']['aggregateRating']
-    poster = data['props']['pageProps']['aboveTheFoldData']['primaryImage']['url']
-    categories = []
-    for category in data['props']['pageProps']['aboveTheFoldData']['genres']['genres']:
-        categories.append(category['text'])
-    trailer = data['props']['pageProps']['aboveTheFoldData']['primaryVideos']['edges'][0]['node']['playbackURLs'][0]['url']
-    description = data['props']['pageProps']['aboveTheFoldData']['plot']['plotText']['plainText']
-    return [ title, year, length, rating, poster, categories, trailer, description ]
+# def getFromImdb(id):
+#     movies = requests.get('http://localhost:3000/movies/' + str(id))
+#     url = movies.json()['imdb-url']
+#     imdb = requests.get(url, headers = headers)
+#     soup = BeautifulSoup(imdb.text, "html.parser")
+#     script = soup.find(id = '__NEXT_DATA__')
+#     data = json.loads(script.get_text())
+#     title = data['props']['pageProps']['aboveTheFoldData']['originalTitleText']['text']
+#     year = data['props']['pageProps']['aboveTheFoldData']['releaseYear']['year']
+#     length = data['props']['pageProps']['aboveTheFoldData']['runtime']['displayableProperty']['value']['plainText']
+#     rating = data['props']['pageProps']['aboveTheFoldData']['ratingsSummary']['aggregateRating']
+#     poster = data['props']['pageProps']['aboveTheFoldData']['primaryImage']['url']
+#     categories = []
+#     for category in data['props']['pageProps']['aboveTheFoldData']['genres']['genres']:
+#         categories.append(category['text'])
+#     trailer = data['props']['pageProps']['aboveTheFoldData']['primaryVideos']['edges'][0]['node']['playbackURLs'][0]['url']
+#     description = data['props']['pageProps']['aboveTheFoldData']['plot']['plotText']['plainText']
+#     return [ title, year, length, rating, poster, categories, trailer, description ]
