90985ce787608ca0c5a21428e9942840d665b46d
Robin Bezak <76775905+bezakrobin@users.noreply.github.com>
2023-02-17 02:13:34 +0100

    responsive swiper for all devices

--- website/static/js/functions.js
+++ website/static/js/functions.js
@@ -13,3 +13,50 @@ function toggleVolume(e) {
         trailer.muted = true;
     }
 }
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 6,
+    slidesPerGroup: 6,
+    spaceBetween: 0,
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        100: {
+          slidesPerView: 2,
+          slidesPerGroup: 1,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 3,
+          slidesPerGroup: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 4,
+          slidesPerGroup: 3,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 6,
+          slidesPerGroup: 6,
+          spaceBetween: 10,
+        },
+    }
+});
+
+var swiperSlide = document.getElementsByName("swiper-slide");
+swiperSlide.forEach(element => {
+    element.addEventListener("mouseenter", () => {   
+        element.querySelector('.bottom-left').setAttribute('class', 'bottom-left white');
+    });
+});
+
+swiperSlide.forEach(element => {
+    element.addEventListener("mouseleave", () => {   
+        element.querySelector('.bottom-left').setAttribute('class', 'bottom-left transparent');
+    });
+});
